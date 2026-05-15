ccc9047a4271cb83721f491f645244fd8139e6c2
Paulothejungle <rivalhermawan47@gmail.com>
2026-05-15 14:04:47 +0700

    style: optimize mobile responsive layout

--- public/js/auth.js
+++ public/js/auth.js
@@ -181,7 +181,7 @@ function renderNavbar(activePage) {
             <polyline points="16 17 21 12 16 7"></polyline>
             <line x1="21" y1="12" x2="9" y2="12"></line>
           </svg>
-          Logout
+          <span class="logout-text">Logout</span>
         </button>
       </div>
     </nav>
